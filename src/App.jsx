daf1af4249b841642fcff62314f991439999db17
aniketchawardol <aniketchawardol@gmail.com>
2024-10-08 22:41:14 +0530

    configured redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import React, {useState, useEffect} from 'react'
+import {login, logout} from './store/authSlice'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+const [loading, setLoading] = useState(true)
+const dispatch = useDispatch()
 
+useEffect(() => {
+ authService.getCurrentUser()
+ .then((userData) => {
+    if (userData) {
+      dispatch(login(userData))
+    }
+    else {
+      dispatch(logout())
+    }
+ })
+ .finally(() => setLoading(false))
+}, [])
 
-  return (
-    <>
-      <h1>blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet />  */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
