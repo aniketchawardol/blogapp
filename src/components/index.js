f1ad9451b387eb80950fbcf4ecb807a777de79dc
aniketchawardol <aniketchawardol@gmail.com>
2024-11-29 21:06:44 +0530

    update README with project details, installation instructions, and usage guidelines; minor formatting adjustments in PostForm component

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,8 @@ import Footer from './Footer/Footer';
 import Container from './Container/Container';
 import Logo from './Logo/Logo';
 import LogoutBtn from './Header/LogoutBtn';
-
+import RTE from './RTE';
 export { Header, Footer,
   Container, Logo,
-  LogoutBtn
+  LogoutBtn,RTE
  };
