57fe8c4845caa67b05ef40fe5f7603d6a4bf504f
aniketchawardol <aniketchawardol@gmail.com>
2024-12-13 23:04:28 +0530

    refactor: improve layout and formatting in App, AddPost, AllPosts, and RTE components; enhance Appwrite service methods

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="h-full w-full flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
         <main><Outlet /></main>
--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
-import conf from "../conf/conf"
+import conf from "../conf/conf";
 
 export default function RTE({
   control,
@@ -19,17 +19,12 @@ export default function RTE({
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            initialValue={defaultValue}
             apiKey={conf.tinyMCEKey}
+            initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
               height: 500,
               menubar: true,
-              plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table paste code help wordcount",
-              ],
               toolbar:
                 "undo redo | formatselect | bold italic backcolor | \
             alignleft aligncenter alignright alignjustify | \
